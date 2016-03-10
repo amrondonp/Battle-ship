fa81a8ecda7c47cb842f6dc8a0868e21cad15268
amrondonp <amrondonp@gmail.com>
2016-03-10 00:20:45 -0500

    Add more beatiuful GUI

--- BattleShip/src/main/Main.java
+++ BattleShip/src/main/Main.java
@@ -2,12 +2,18 @@ package main;
 import controller.Controller;
 import controller.SimpleClientController;
 
+import javax.swing.*;
+
 
 public class Main {
 	public static void main(String[] args){
-		
-		
-			Controller controller = Controller.getInstance();
+
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		Controller controller = Controller.getInstance();
 			SimpleClientController.getInstance().getClient();
 			//controller.getTheView().getFramePlayer().setVisible(true);
 			//controller.getTheView().getFrameOpponent().setVisible(true);
